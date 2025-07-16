c01303a1c9f3e87fad1ecc632ed3a86a97315d68
Egemen Çakır <cakiregemen0@gmail.com>
2025-07-16 20:31:51 +0300

    backend last update

--- backend/src/main/java/com/gezerkengor/GezerkengorApplication.java
+++ backend/src/main/java/com/gezerkengor/GezerkengorApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GezerkengorApplication {
-
+ 
 	public static void main(String[] args) 
 	{
 		SpringApplication.run(GezerkengorApplication.class, args);
 	}
 
 }    
-       
+         
